1b0b6ad40e77d45136ee44b9f2e3727f92a9ff3f
Jaehwan Kim <jaehwkim@c2r2s1.42seoul.kr>
2022-04-25 19:00:11 +0900

    blue print

--- pipex.c
+++ pipex.c
@@ -6,11 +6,26 @@
 /*   By: jaehwkim <jaehwkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 15:14:30 by jaehwkim          #+#    #+#             */
-/*   Updated: 2022/04/25 16:18:45 by jaehwkim         ###   ########.fr       */
+/*   Updated: 2022/04/25 18:59:57 by jaehwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void	child_pro(int *fd, char **argv, char **envp)
+{
+	int	infile;
+
+	infile = open(argv[1], O_RDONLY);
+	if (infile == -1)
+		error_exit();
+	if (dup2(infile, STDIN_FILENO) == -1)
+		error_exit();
+	if (dup2(infile, STDOUT_FILENO)== -1)
+		error_exit();
+	close(fd[0]);
+	// exec_cmd(argv[2], envp);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -25,9 +40,9 @@ int	main(int argc, char **argv, char **envp)
 		if (pid == -1)
 			return (1);
 		if (pid == 0)
-			child(fd, argv, envp);
+			child_pro(fd, argv, envp);
 		else if (pid > 0)
-			parent(fd, argv, envp);
+			parent_pro(fd, argv, envp);
 	}
 	else
 		arg_error();
