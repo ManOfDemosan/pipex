87dc8821cf4634cdb34b6d2661cc8451d0334564
Jaehwan Kim <jaehwkim@c4r3s1.42seoul.kr>
2022-04-26 15:41:09 +0900

    blue print 2

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: jaehwkim <jaehwkim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 15:48:20 by jaehwkim          #+#    #+#             */
-/*   Updated: 2022/04/26 13:55:02 by jaehwkim         ###   ########.fr       */
+/*   Updated: 2022/04/26 14:38:25 by jaehwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@ void	error_exit(void)
 	exit(EXIT_FAILURE);
 }
 
-void	make_stream(char *av, char **envp)
+char	*check_access(char *cmd, char **envp)
+{
+}
+
+void	make_stream(char *cmd char **envp)
 {
 	char	**argv;
-	char	*real_av;
+	char	*real_cmd;
 
-	argv = ft_split(av, ' ');
-	real_av = check_access(argv[0], envp);
-	if(real_av == 0)
+	argv = ft_split(cmd, ' ');
+	real_cmd = check_access(argv[0], envp);
+	if (real_cmd == 0)
 		error_exit();
-	execve(real_av, argv, envp);	
+	execve(real_cmd, argv, envp);
 }
 
 int	arg_error(void)
